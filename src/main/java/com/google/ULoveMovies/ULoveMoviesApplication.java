46942d0c610ddf5a0188390ba2918d15a9d4ecc0
Sanjai <sanjaiarivazhagan03@gmail.com>
2025-11-29 11:23:16 +0530

    first commit

---
+++ src/main/java/com/google/ULoveMovies/ULoveMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.google.ULoveMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ULoveMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ULoveMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/ULoveMovies/ULoveMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.ULoveMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ULoveMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
